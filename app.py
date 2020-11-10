767bcec806ca3ab18a68208d963bed7ae2c3c9e4
Colin Edwards <cedwards@curioanalytics.com>
2020-11-10 11:37:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from profit_df import profit_summary
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 @st.cache
 def get_df():
@@ -15,10 +15,3 @@ df1.loc['Total',['cg', 'cp', 'profit', 'mw']] = df1.sum(axis=0, skipna=True)
 for col in ['cg', 'cp', 'profit']:
     df[col] = df[col].apply(lambda x: '${0:,.0f}'.format(x).replace('$-', '-$'))
 st.dataframe(df1)
-
-
-
-
-
-
-
