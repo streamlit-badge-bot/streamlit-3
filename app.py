6d8c9542c1b8884229b5a1d5aeeb14b768d1aa91
Colin Edwards <cedwards@curioanalytics.com>
2020-11-10 12:06:16 -0800

    Update app.py
    Added logo, fixed $ formatting

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ import matplotlib.pyplot as plt
 @st.cache
 def get_df():
     return profit_summary()
-df = get_df().set_index(['date_ms'])
+df0 = get_df().set_index(['date_ms'])
+
+
+st.image('curio_logo.png', use_column_width=True)
 
 # filtered df
-option = st.sidebar.selectbox('Competitor', df['competitor_name'].unique())
-df1 = df[df['competitor_name'] == option]
-df1.loc['Total',['cg', 'cp', 'profit', 'mw']] = df1.sum(axis=0, skipna=True)
+df = df0.copy()
+option = st.selectbox('Competitor', df['competitor_name'].unique())
+df = df[df['competitor_name'] == option]
+df.loc['Total', ['cg', 'cp', 'profit', 'mw']] = df.sum(axis=0, skipna=True)
 for col in ['cg', 'cp', 'profit']:
     df[col] = df[col].apply(lambda x: '${0:,.0f}'.format(x).replace('$-', '-$'))
-st.dataframe(df1)
+st.dataframe(df)
